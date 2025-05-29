378c81f344a4c1263ec0a6a4ce704cba67e18906
Hanielly <165221030+hannymarques@users.noreply.github.com>
2025-05-29 12:54:28 -0300

    Update RevistaCientifica.java

--- PublicacaoCientifica/src/RevistaCientifica.java
+++ PublicacaoCientifica/src/RevistaCientifica.java
@@ -2,31 +2,29 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class RevistaCientifica {
-    private String nome;
     private List<Observer> assinantes = new ArrayList<>();
+    private String nome;
 
-    public RevistaCientifica(String nome) {
+    public RevistaCientifica(String nome){
         this.nome = nome;
     }
 
-    public void adicionarAssinante(Observer o) {
-        if (o != null && !assinantes.contains(o)) {
-            assinantes.add(o);
-        }
+    public void adicionarAssinante(Observer assinante){
+        assinantes.add(assinante);
     }
 
-    public void removerAssinante(Observer o) {
-        assinantes.remove(o);
+    public void removerAssinante(Observer assinante){
+        assinantes.remove(assinante);
     }
 
-    public void publicar(String publicacao) {
-        System.out.println("Revista " + nome + " publicou: " + publicacao);
-        notificar(publicacao);
+    public void publicar(String conteudo){
+        System.out.println("Revista " + nome + " publicou: " + conteudo);
+        notificarAssinantes(conteudo);
     }
 
-    private void notificar(String publicacao) {
-        for (Observer o : assinantes) {
-            o.update(nome, publicacao);
+    private void notificarAssinantes(String conteudo){
+        for(Observer assinante : assinantes){
+            assinante.update(nome, conteudo);
         }
     }
 }
